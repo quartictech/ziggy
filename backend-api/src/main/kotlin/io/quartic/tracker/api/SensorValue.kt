dc2d8ef2d512d14a1b7bc9133c6db64d2b2c2f82
Alex Sparrow <alex@quartic.io>
2017-01-04 18:32:40 +0000

    Crappy fake API

---
+++ backend-api/src/main/kotlin/io/quartic/tracker/api/SensorValue.kt
@@ -0,0 +1,3 @@
+package io.quartic.tracker.api
+
+data class SensorValue(val id: Int, val name: String, val value: String, val timestamp: Long)
--- backend/src/main/kotlin/io/quartic/tracker/TrackerResource.kt
+++ backend/src/main/kotlin/io/quartic/tracker/TrackerResource.kt
@@ -3,12 +3,14 @@ package io.quartic.tracker
 import io.quartic.common.logging.logger
 import io.quartic.tracker.api.RegistrationRequest
 import io.quartic.tracker.api.RegistrationResponse
+import io.quartic.tracker.api.UploadRequest
 import java.util.*
 import javax.ws.rs.Consumes
 import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
 
 class TrackerResource {
     private val LOG by logger()
@@ -22,4 +24,15 @@ class TrackerResource {
         LOG.info("$request -> $response")
         return response
     }
+
+    @POST
+    @Path("/upload")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    fun upload(request: UploadRequest): Response {
+        val response = Response.ok().build()
+        LOG.info("$request -> $response")
+        return response
+    }
+
 }
