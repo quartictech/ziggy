bd83d87c3b1875c3143b37c1008af1b7d3afffc5
Alex Sparrow <alex@quartic.io>
2017-01-27 10:12:53 +0000

    Centralize shared preference writing

--- app/src/main/kotlin/io/quartic/app/state/ApplicationState.kt
+++ app/src/main/kotlin/io/quartic/app/state/ApplicationState.kt
@@ -4,11 +4,14 @@ import android.accounts.Account
 import android.accounts.AccountManager
 import android.content.Context
 import android.content.Context.ACCOUNT_SERVICE
+import android.content.SharedPreferences
+import android.util.Log
 import io.quartic.app.ApplicationConfiguration
 import io.quartic.app.api.BackendApi
 import io.quartic.app.authHttpClient
 import io.quartic.app.clientOf
 import io.quartic.app.storage.Database
+import io.quartic.app.tag
 import okhttp3.OkHttpClient
 
 class ApplicationState(val context: Context, val configuration: ApplicationConfiguration) {
@@ -18,20 +21,30 @@ class ApplicationState(val context: Context, val configuration: ApplicationConfi
         }
 
         const val ACCOUNT_TYPE = "io.quartic.remote"
+        const val PREFERENCES_NAME = "remote"
+        val TAG by tag()
     }
 
-    private val sharedPreferences = context.getSharedPreferences("tracker", 0)
+    private val sharedPreferences = context.getSharedPreferences(PREFERENCES_NAME, 0)
 
+
+    private fun <R> checkedEdit(block: SharedPreferences.Editor.() -> R) {
+        val editor = sharedPreferences.edit()
+        editor.block()
+
+        // commit() applies edits synchronously and returns an exit status
+        if (!editor.commit()) {
+            Log.e(TAG, "exception while committing to shared preferences")
+        }
+    }
 
     fun clear() {
-        sharedPreferences.edit().clear().apply()
+        checkedEdit { clear() }
     }
 
     var userId: String?
         get() = sharedPreferences.getString("userId", null)
-        set(userId) = sharedPreferences.edit()
-                .putString("userId", userId)
-                .apply()
+        set(userId) = checkedEdit { this.putString("userId", userId) }
 
     val client: BackendApi
         get() = clientOf(configuration.backendBaseUrl, OkHttpClient())
@@ -53,16 +66,12 @@ class ApplicationState(val context: Context, val configuration: ApplicationConfi
             accountManager.addAccountExplicitly(account, null, null)
             return account
         }
+
     var lastSyncTime: Long
         get() = sharedPreferences.getLong("lastSyncTime", 0)
-        set(timestamp) = sharedPreferences.edit()
-                .putLong("lastSyncTime", timestamp)
-                .apply()
+        set(timestamp) = checkedEdit { putLong("lastSyncTime", timestamp) }
 
     var lastAttemptedSyncTime: Long
         get() = sharedPreferences.getLong("lastAttemptedSyncTime", 0)
-        set(timestamp) { sharedPreferences.edit()
-                .putLong("lastAttemptedSyncTime", timestamp)
-                .commit() }
-
+        set(timestamp) = checkedEdit { putLong("lastAttemptedSyncTime", timestamp) }
 }
